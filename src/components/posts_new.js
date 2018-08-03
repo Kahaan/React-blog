83a4daa86ebd3cea23096c5acb02aea5c805fce7
Kahaan <kahaan.r.patel@gmail.com>
2018-08-03 11:37:20 -0700

    react-redux

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,15 +1,66 @@
 import React, {Component} from 'react';
-
+import {Field, reduxForm } from 'redux-form'
 
 class PostsNew extends  Component{
 
+  renderField(field){
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+          />
+      </div>
+    )
+  }
+
+
   render() {
     return (
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     )
   }
 }
 
-export default PostsNew
+function validate(values){
+  const errors = {}
+
+  if (!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is atleast 3 chars!"
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter a category!"
+  }
+
+  if (!values.content) {
+    errors.content = "Enter content!"
+  }
+
+
+  return errors
+
+}
+
+export default reduxForm({
+  validate,
+  form:'PostsNewForm'
+})(PostsNew)
